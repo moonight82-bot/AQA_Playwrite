ae6f5912f9ac40ad3290cf3d46fb3b36929667f5
Valentyna <moonight82@gmail.com>
2026-06-08 21:58:20 +0300

    Update for Firefox

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,18 @@ export default defineConfig({
     },
     dependencies: ["setup"],
   },
-]
+
+  {
+    name: "firefox",
+    testDir: "./tests",
+    testMatch: /.*\.spec\.ts/,
+    use: {
+      ...devices["Desktop Firefox"],
+      baseURL: process.env.BASE_URL,
+      storageState: ".auth/user.json",
+    },
+    dependencies: ["setup"],
+  },
+],
 
 });
